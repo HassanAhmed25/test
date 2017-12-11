78fd8849e954ebde16f0491c8826e6e6e3ba4d0d
HassanAhmed25 <hassanahmed2569@gmail.com>
2017-12-12 02:39:49 +0500

    tes

---
+++ factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Number:     ");
+            int fact = Convert.ToInt32(Console.ReadLine());
+            for (int i = fact - 1 ; i >= 1; i--)
+            {
+                fact = fact * i;
+            }
+            Console.Write("Factorial is:    " + fact);
+            Console.ReadKey();
+
+        }
+    }
+}
